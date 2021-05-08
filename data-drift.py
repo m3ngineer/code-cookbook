7130eeeb90b1eaa96f4bf388e23c14196955f33f
Matthew Eng <matthew.eng2@gmail.com>
2021-05-07 22:22:00 -0400

    Add data-drift.py

---
+++ data-drift.py
@@ -0,0 +1,22 @@
+# Automatically checks for changes in data upon code changes
+
+# Inputs: 2 dataframes for comparison. 1 dataframe could be the data used to train a model. The other is the current data used to make predictions
+
+# Ouput: A series of tests that dectect statistical differences and changes in the dataset
+
+class DriftTest():
+
+    def __init__(self):
+        pass
+
+    def test(self):
+        pass
+
+    def test_mean(self):
+        pass
+
+    def test_median(self):
+        pass
+
+    def test_mean(self):
+        pass
